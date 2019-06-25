591924b0f31451403c6a06838c9eb048f5ee95e4
Andreas Karis <ak.karis@gmail.com>
2019-06-25 10:05:21 -0400

    Update generate_scripts.sh

--- generate_scripts.sh
+++ generate_scripts.sh
@@ -4,10 +4,16 @@ JINJA=$(which jinja2)
 
 if [ "$JINJA" == "" ]; then
   echo "Cannot find jinja2 binary. Please install jinja2"
-  echo "E.g.:"
+  echo "E.g. on fedora:"
   echo "sudo yum install python3-jinja2-cli.noarch"
   echo "or"
   echo "sudo yum install python2-jinja2-cli.noarch"
+  echo "E.g. on RHEL:"
+  echo "subscription-manager repos --enable=rhel-7-server-rpms"
+  echo "yum install python-virtualenv -y"
+  echo "virtualenv jinja2-cli"
+  echo "source jinja2-cli/bin/activate"
+  echo "pip install jinja2-cli"
   exit 1
 fi
 
