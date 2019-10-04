107aa5d951e0a12d538927a5fe05d869f7c1b999
Andreas Karis <ak.karis@gmail.com>
2019-10-04 18:10:02 +0200

    Image tag - sed replace with 'g'
    Needed to string replace all occurrences in case several containers use the same image

--- templates/start_hotfix_container.j2.sh
+++ templates/start_hotfix_container.j2.sh
@@ -14,7 +14,7 @@ HOTFIX_IMAGE={{ container_registry }}/{{ container_image_name }}:$HOTFIX_TAG
 mkdir /tmp/docker_backup
 cp /var/lib/tripleo-config/docker-container-startup-config-step_4.json /tmp/docker-container-startup-config-step_4.${HOTFIX_TAG}.json
 
-sed -i "s#\"${ORIGINAL_IMAGE}\"#\"${HOTFIX_IMAGE}\"#" /tmp/docker-container-startup-config-step_4.${HOTFIX_TAG}.json
+sed -i "s#\"${ORIGINAL_IMAGE}\"#\"${HOTFIX_IMAGE}\"#g" /tmp/docker-container-startup-config-step_4.${HOTFIX_TAG}.json
 
 docker stop {{ container_name }}
 paunch debug --action run --file /tmp/docker-container-startup-config-step_4.${HOTFIX_TAG}.json --container {{ container_name }}
